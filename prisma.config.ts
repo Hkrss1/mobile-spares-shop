6fda54f1c40fcc800e7fc47f1d222491cddbdfd0
Ankit Raj <ankit512002@gmail.com>
2025-11-21 20:49:50 +0530

    Critical fix: Use DATABASE_URL env var

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: env("postgresql://postgres:Hkr@brand1@db.hvikddcstxohawqmreho.supabase.co:5432/postgres"),
+    url: env("DATABASE_URL"),
   },
 });
